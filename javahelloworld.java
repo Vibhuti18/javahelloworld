37791f21a35b848c485d38bbc149ca3fe043249f
vibhuti <vibhuti_sawant@persistent.co.in>
2018-02-01 03:46:41 +0000

    My first commit

---
+++ javahelloworld.java
@@ -0,0 +1,14 @@
+public class javahelloworld
+{
+
+
+public static void main(String args[])
+{
+
+System.out.println("Hello wolrld For the first time");
+System.out.println("Hello once again");
+
+
+}
+
+}
